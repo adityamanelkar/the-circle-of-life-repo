e54d0cad69cb0119be08943a0d3f206ad311d11f
Anmol8991 <anmol.arora8991@gmail.com>
2022-10-22 04:16:09 -0400

    Updated Agent file
    Added a move function

--- agent.py
+++ agent.py
@@ -19,7 +19,7 @@ class Agent():
         self.name = name
         self.node = random.randint(0,49)
 
-        # print("Agent Position at spawn: "+str(self.node))
+        print("Initial Agent Position: "+str(self.node))
 
 
     def move_1(self, graph, prey_pos, pred_pos) -> None:
@@ -28,75 +28,90 @@ class Agent():
 
         """
 
-        # print("Prey Position: "+str(prey_pos))
-        # print("Predator Position:  "+str(pred_pos))
+
 
         # Shortest Distance from Agent to prey
         agent_to_prey = nx.shortest_path_length(graph,source = self.node, target = prey_pos)
+        print ("Distance of Agent to Prey: "+str(agent_to_prey))  
+
 
         # Shortest Distance from Agent to Predator
         agent_to_predator = nx.shortest_path_length(graph,source = self.node, target = pred_pos)
+        print ("Distance of Agent to Predator: "+str(agent_to_predator))
+
 
         # List of Neighbors of Agent
         agent_neighbours = list(graph.neighbors(self.node))
+        print()
+        print("Neighbors of Agent: {}".format(agent_neighbours))
 
-        # print ("Distance of Agent to Prey: "+str(agent_to_prey))
-        # print ("Distance of Agent to Predator: "+str(agent_to_predator))
+        
+        
 
-        # Order which the Agent follows
-        priority = [None]*len(agent_neighbours)
+        # Setting priority for every neighbor according to the order followed by Agent1 
+        priority = {}
+
+        # priority = [None]*len(agent_neighbours)
 
         for i in range(len(agent_neighbours)):
             # dist = []
 
-            # Distance of shortest path  from neighbor to prey
+            # Distance of shortest path from neighbor to prey
             neighbor_to_prey = nx.shortest_path_length(graph,source = agent_neighbours[i], target=prey_pos)
 
             # dist.append(neighbor_to_prey)
 
-            # Distance of shortest path  from neighbor to predator
+            # Distance of shortest path from neighbor to predator
             neighbor_to_predator = nx.shortest_path_length(graph,source = agent_neighbours[i], target=pred_pos)
 
             # dist.append(neighbor_to_predator)
 
 
             if neighbor_to_prey < agent_to_prey and neighbor_to_predator > agent_to_predator:
-                priority[i] = 1
+                priority[agent_neighbours[i]] = 1
+                # priority[i] = 1
             elif neighbor_to_prey < agent_to_prey and neighbor_to_predator == agent_to_predator :
-                priority[i] = 2
+                priority[agent_neighbours[i]] = 2
+                # priority[i] = 2
             elif neighbor_to_prey == agent_to_prey and neighbor_to_predator > agent_to_predator :
-                priority[i] = 3
+                priority[agent_neighbours[i]] = 3
+                # priority[i] = 3
             elif neighbor_to_prey == agent_to_prey and neighbor_to_predator == agent_to_predator :
-                priority[i] = 4 
+                priority[agent_neighbours[i]] = 4
+                # priority[i] = 4 
             elif neighbor_to_predator > agent_to_predator :
-                priority[i] = 5
+                priority[agent_neighbours[i]] = 5
+                # priority[i] = 5
             elif neighbor_to_predator == agent_to_predator :
-                priority[i] = 6
+                priority[agent_neighbours[i]] = 6
+                # priority[i] = 6
             else :
-                priority[i] = 7
+                priority[agent_neighbours[i]] = 7
+                # priority[i] = 7
 
         #     print("Distance of neighbor {} to prey and predator".format(agent_neighbours[i]))
         #     print(dist)
         #     print("priority of neighbor "+str(agent_neighbours[i]))
         #     print(priority[i])
-        # print(priority)
+        print("Priority of every neighbour:")
+        print(priority)
 
-        # List of neighbors with highest priority
-        n_highest_priority = []
+        
+        
 
         # Highest priority value out of all the neighbors
-        m = min(priority)
+        min_val = min(priority.values())
 
         # print("Highest priority value out of all the neighbors: "+str(m))
 
         # neighbors with the Highest priority
-        if m < 7:
-            for i,j in enumerate(priority):
-                if j == m:
-                    n_highest_priority.append(agent_neighbours[i])                    
-            # Breaking ties at random
-            next_pos = random.choice(n_highest_priority)
+        if min_val < 7:
+            # List of neighbors with highest priority
+            Highest_priority_neighbors = [key for key, value in priority.items() if value == min_val]      
 
+            # Breaking ties at random
+            next_pos = random.choice(Highest_priority_neighbors)
+            # Update node for the agent
             self.node = next_pos
         else:
             self.node = self.node
