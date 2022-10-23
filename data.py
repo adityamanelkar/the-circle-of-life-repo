f4e824ed9b1305ce9bd4424236a9ab00adaa74a1
Aditya Manelkar <adityamanelkar@Adityas-MacBook-Pro.local>
2022-10-23 14:36:46 -0400

    Updated data.py
    Fixed some bugs and added the feature to save the plot directly as a PNG instead of showing it

--- data.py
+++ data.py
@@ -1,41 +1,46 @@
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def ploting (l1,l2,l3, maxRuns, agentNum):
-    x = [i for i in range(maxRuns)]
-    y = l1
-    plt.plot(x, y, marker='o', color='b',label='success_rate')
+def plotting(l1, l2, l3, num_runs, num_trials, agent_num):
+
+    # X
+    x = [i for i in range(num_runs)]
     plt.xticks(x)
-    # for i, j in zip(x, y)
-    #     plt.text(i, j + 0.005, "({}, {})".format(i, round(j, 2)))
-    y = l2
-    plt.plot(x, y, marker='o', color='r',label='failure_rate_predator')
 
-    y = l3
-    plt.plot(x, y, marker='o', color='b',label='timeout_rate')
-    plt.xlabel('Number of Runs')
+    # Plots for success/failure rates
+    plt.plot(x, l1, marker='o', color='b', label='success_rate')
+    plt.plot(x, l2, marker='o', color='r', label='failure_rate_predator')
+    plt.plot(x, l3, marker='o', color='y', label='timeout_rate')
+
+    # Labels and legend
+    plt.xlabel('Run Numbers')
     plt.ylabel('Average Rate')
-    plt.title("AGENT {} (Tested on {} graphs for each run)".format(agentNum, len(l1)))
-    plt.show()
+    plt.title("AGENT {} (Tested on {} trials for each run)".format(agent_num, num_trials))
+    plt.legend()
 
+    # Just save a PNG for the plot
+    plt.gcf().set_size_inches(11.2775330396, 7.04845814978) # The MacbookPro 13.3 inches size
+    plt.savefig("./graphs/agent{}.png".format(agent_num), dpi=227) # The MacbookPro 13.3 inches dpi
+
+agent_num = int(input("Enter the agent number: "))
+num_runs = int(input("Enter the number of runs for which you want to cram data: "))
 
 agent_success = []
 failure_pred = []
 failure_timeout = []
+num_trials = 0
 
-agentNum = int(input("Enter the agent number: "))
-maxRuns = int(input("Enter the max number of ghosts that you want to cram data for: "))
-
-
-for i in range(maxRuns):
-    filepath = "./agent_csv/agent" + str(agentNum) + "/Run" + str(i) + '.csv'
+for run in range(num_runs):
+    filepath = "./agent_csv/agent" + str(agent_num) + "/Run" + str(run) + '.csv'
     df = pd.read_csv(filepath)
     success_rate = df["success"].mean()
     agent_success.append(success_rate)
     failure_rate = df["failure_predator"].mean()
-    failure_pred.append(success_rate)
-    timeout_rate = df["Failure_Timeout"].mean()
-    failure_timeout.append(success_rate)
+    failure_pred.append(failure_rate)
+    timeout_rate = df["failure_timeout"].mean()
+    failure_timeout.append(timeout_rate)
 
+    if num_trials == 0:
+        num_trials = len(df)
     
-ploting(agent_success,failure_pred,failure_timeout,maxRuns,agentNum)
+plotting(agent_success, failure_pred, failure_timeout, num_runs, num_trials, agent_num)
