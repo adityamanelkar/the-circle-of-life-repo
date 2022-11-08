cfa20ad8a1dc3114a19e23af856590efd2af66fc
Aditya Manelkar <adityamanelkar@adityas-mbp.mynetworksettings.com>
2022-11-07 22:47:16 -0500

    Updated predator.py
    Updated the movement logic and added a new logic for distracted predator

--- predator.py
+++ predator.py
@@ -1,4 +1,5 @@
 import random
+import math
 from graph_utils import Graph
 from agent import Agent
 
@@ -16,11 +17,9 @@ class Predator:
     def move_predator(self, graph: Graph, agent: Agent) -> None:
         """
         Move the predator to the most favorable neighbor to catch the agent
-        
-        The predator could choose to stay if that may be the best option
         """
-        best_neighbors = [self.pos]
-        shortest_path_length = graph.shortest_path_length(self.pos, agent.node)
+        best_neighbors = []
+        shortest_path_length = math.inf
 
         for neighbor in graph.neighbors(self.pos):
             
@@ -35,3 +34,31 @@ class Predator:
 
         # Just set the current position to the best neighbor (and break ties randomly if there is more than one)
         self.pos = random.choice(best_neighbors)
+
+    def move_distracted_predator(self, graph: Graph, agent: Agent) -> None:
+        """
+        Move the predator to the most favorable neighbor to catch the agent with a probability of 0.6,
+        OR randomly move it one of its neighbors with a probability of 0.4
+        """
+        best_neighbors = []
+        shortest_path_length = math.inf
+
+        for neighbor in graph.neighbors(self.pos):
+            
+            path_length = graph.shortest_path_length(neighbor, agent.node)
+            
+            if path_length < shortest_path_length:
+                best_neighbors = [neighbor] # this is done because we have a clear cut best neighbor
+                shortest_path_length = path_length
+            
+            elif path_length == shortest_path_length:
+                best_neighbors.append(neighbor) # this is done because we have a tie which we need to break randomly
+
+        all_neighbors = graph.neighbors(self.pos)
+
+        if random.random() <= 0.6:
+            # Just set the current position to the best neighbor (and break ties randomly if there is more than one)
+            self.pos = random.choice(best_neighbors)
+        else:
+            # Just randomly select any node
+            self.pos = random.choice(all_neighbors)
